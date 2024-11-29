89992d2131327e1507a13836a30c0d496707cbce
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2024-11-29 17:55:30 -0600

    Point and Line classes

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from Window import Window
+from PointLine import Point, Line
 
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(50, 50), Point(150, 150))
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 
